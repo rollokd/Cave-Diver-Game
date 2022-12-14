c70a45f9a86cf96553b971f3a28696ae4369d776
Ben <ben250402@yahoo.co.uk>
2022-12-14 18:45:47 +0000

    Added animations for jumping and running and idle

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,16 @@ public class PlayerMovement : MonoBehaviour
     public float movementSpeed;
     public float jumpSpeed;
     public Rigidbody2D rb;
+    public bool sidewaysMovement;
     
     private bool isGrounded;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,10 +24,19 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y);
 
+        animator.SetBool("Sideways", Mathf.Abs(rb.velocity.x) > 0);
+
         isGrounded = rb.velocity.y < 0.1f;
         Debug.Log(isGrounded);
 
         if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.01f)
+        {
             rb.velocity += Vector2.up * jumpSpeed;
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
     }
 }
