1eb0899446429ef42c16bcad774e00ac586bb17b
Ben <ben250402@yahoo.co.uk>
2022-12-30 15:28:08 +0000

    Added code for double jump for boss ai

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -11,12 +11,12 @@ public class Boss : Enemy
     private float maxDistance;
     [SerializeField]
     private LayerMask layerMask;
+    [SerializeField]
+    private float movementSpeed = 3;
 
     private Rigidbody2D rb;
     private float horizontal;
-    private float movementSpeed = 3;
     private float jumpSpeed = 9;
-    private bool canJumpAgain;
 
     // Start is called before the first frame update
     void Start()
@@ -28,31 +28,61 @@ public class Boss : Enemy
     // Update is called once per frame
     void Update()
     {
-        bool isGrounded = Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, maxDistance, layerMask);
-
-        if (isGrounded)
-            canJumpAgain = true;
-
         rb.velocity = new Vector2(horizontal * movementSpeed, rb.velocity.y);
-
-        if ((rb.velocity.x > 0 && !facingRight) || (rb.velocity.x < 0 && facingRight))
-            Flip();
-
-        if (doubleJump && canJumpAgain && !isGrounded && Mathf.Abs(rb.velocity.y) < 0.1f)
-        {
-            Jump();
-            canJumpAgain = false;
-        }
     }
 
     public IEnumerator StartFight()
     {
         for (int i = 0; i < 100; i++)
         {
-            Move(-1, Random.Range(0,5));
-            yield return new WaitForSeconds(Random.Range(0, 5)); 
+            if (doubleJump)
+            {
+                //First block
+                Flip();
+                Move(-1, 3.6f);
+                yield return new WaitForSeconds(2.2f * 3 / movementSpeed);
+                Jump();
+                yield return new WaitForSeconds(2 * 3 / movementSpeed);
+
+                //Second block
+                Flip();
+                Move(1, 3.4f);
+                yield return new WaitForSeconds(0.2f * 3 / movementSpeed);
+                Jump();
+                yield return new WaitForSeconds(1 * 3 / movementSpeed);
+                Jump();
+                yield return new WaitForSeconds(3 * 3 / movementSpeed);
+
+                //Back down
+                Flip();
+                Move(-1, 3.4f);
+                yield return new WaitForSeconds(2.2f * 3 / movementSpeed);
+                Jump();
+                yield return new WaitForSeconds(4 * 3 / movementSpeed);
+
+                //Jump up left
+                Jump();
+                yield return new WaitForSeconds(1 * 3 / movementSpeed);
+                Jump();
+                yield return new WaitForSeconds(0.5f * 3 / movementSpeed);
+                Move(-1, 1f);
+                yield return new WaitForSeconds(2 * 3 / movementSpeed);
+
+                //Back right
+                Flip();
+                Move(1, 4.6f);
+                yield return new WaitForSeconds(5 * 3 / movementSpeed);
+            }
+        }
+    }
+    public IEnumerator StartRandomFight()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Move(-1, Random.Range(0, 5));
+            yield return new WaitForSeconds(Random.Range(0, 5));
             Jump();
-            yield return new WaitForSeconds(Random.Range(0, 5)); 
+            yield return new WaitForSeconds(Random.Range(0, 5));
             Move(1, Random.Range(1, 5));
             yield return new WaitForSeconds(Random.Range(0, 5));
             Jump();
@@ -65,14 +95,13 @@ public class Boss : Enemy
         rb.velocity = new Vector2(rb.velocity.x, 0) + Vector2.up * jumpSpeed;
     }
 
-    private void Move(float horizontalDirection, int time)
+    private void Move(float horizontalDirection, float time)
     {
         horizontal = horizontalDirection;
-
-        StartCoroutine(StopMoving(time));
+        StartCoroutine(StopMoving(time*3/movementSpeed));
     }
 
-    private IEnumerator StopMoving(int time)
+    private IEnumerator StopMoving(float time)
     {
         yield return new WaitForSeconds(time);
         horizontal = 0;
