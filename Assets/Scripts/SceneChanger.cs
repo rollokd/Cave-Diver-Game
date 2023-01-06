ae0b3f727a46789a208124dbeb3a6f4138cc199f
Ben <ben250402@yahoo.co.uk>
2023-01-06 13:37:02 +0000

    Added cutscene for sparing enemy

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneToChangeTo = "Menu";
+    [SerializeField]
+    private float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+            SceneManager.LoadScene(sceneToChangeTo);
+    }
+}
