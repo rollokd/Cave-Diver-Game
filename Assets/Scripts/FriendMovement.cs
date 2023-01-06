b4fad7b60f4fcf8c28c1bb795ee6d11a2f3f8d39
Ben <ben250402@yahoo.co.uk>
2023-01-06 11:51:12 +0000

    Friend animations

--- Assets/Scripts/FriendMovement.cs
+++ Assets/Scripts/FriendMovement.cs
@@ -6,7 +6,17 @@ public class FriendMovement : MonoBehaviour
 {
 
     public PlayerMovement player;
+
     private bool facingRight = true;
+    private Animator animator;
+    private Animator playerAnimator;
+    private Vector3 prevPos;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        playerAnimator = player.GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -19,6 +29,14 @@ public class FriendMovement : MonoBehaviour
 
         gameObject.transform.position = new Vector2(player.transform.position.x, gameObject.transform.position.y);
         gameObject.transform.localScale = player.transform.localScale;
+
+        Vector3 pos = transform.position;
+
+        Debug.Log(pos);
+        Debug.Log(pos != prevPos);
+        animator.SetBool("Sideways", playerAnimator.GetBool("Sideways"));
+
+        prevPos = pos;
     }
 
     public void Flip()
